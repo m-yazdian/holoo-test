4832f8b0888eef3a483744de82a3953fb748b7f6
Mohammad Yazdian <m.yazdian70@outlook.com>
2023-09-23 21:10:02 +0330

    Adding base interface for DTO classes

---
+++ commons/src/main/java/ir/co/holoo/commons/dto/GeneralDto.java
@@ -0,0 +1,11 @@
+package ir.co.holoo.commons.dto;
+
+import java.io.Serializable;
+
+/**
+ * A marker interface for Data Transfer Object (DTO) layers.
+ *
+ * @author Mohammad Yazdian
+ */
+public interface GeneralDto extends Serializable {
+}
